6c0ab1f811853db1d8929f460aa14da73d02aa6e
jmcfun <jmcfunonline@gmail.com>
2025-11-13 12:14:10 +0100

    feat: add username and plan to users

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,14 +32,17 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
+            'username' => ['required', 'string', 'max:50', 'alpha_num', 'unique:users,username'],
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'plan' => 'free',
         ]);
 
         event(new Registered($user));
