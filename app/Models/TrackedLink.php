d893f60f061f7975f4ceb95d0dd41156d01c34da
jmcfun <jmcfunonline@gmail.com>
2025-11-20 12:22:41 +0100

    chore: auto-generate tracked link short codes

--- app/Models/TrackedLink.php
+++ app/Models/TrackedLink.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Support\Links\ShortCode;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,6 +17,19 @@ class TrackedLink extends Model
         'tracking_key',
         'short_code',
     ];
+
+    protected static function booted(): void
+    {
+        static::created(function (TrackedLink $trackedLink) {
+            if ($trackedLink->short_code) {
+                return;
+            }
+
+            $trackedLink->short_code = ShortCode::encode($trackedLink->id);
+            // Extra save only happens once to backfill older entries and factory-created rows.
+            $trackedLink->save();
+        });
+    }
 
     // Relations
 
