44a6f6b803f496c8736086ce184ba98349118e11
jmcfun <jmcfunonline@gmail.com>
2025-11-21 17:15:59 +0100

    fix: app error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,7 +33,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->respond(function (\Illuminate\Http\Response $response, Throwable $exception, \Illuminate\Http\Request $request) {
+        $exceptions->respond(function (\Symfony\Component\HttpFoundation\Response $response, Throwable $exception, \Illuminate\Http\Request $request) {
             if (! app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
                 return \Inertia\Inertia::render('Error', ['status' => $response->getStatusCode()])
                     ->toResponse($request)
